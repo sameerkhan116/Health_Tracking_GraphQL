2d147984958ed9d0962a6db73c479d920c556d0c
Sameer Khan <sameer_khan@student.uml.edu>
2018-10-17 21:38:51 -0400

    Added component with types :win:

---
+++ react-client/src/Home.tsx
@@ -0,0 +1,107 @@
+// @ts-ignore
+import { withAuth } from '@okta/okta-react';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { Button, Container } from 'reactstrap';
+import AppNavbar from './AppNavbar';
+
+interface IUser {
+  given_name: string;
+}
+
+interface State {
+  authenticated: boolean | any;
+  userinfo: IUser | null;
+  isOpen: boolean;
+}
+
+interface Props {
+  isAuthenticated: () => boolean;
+  getUser: () => IUser;
+  auth: {
+    login: (path: string) => void;
+    logout: (path: string) => void;
+  };
+}
+
+export default withAuth(
+  class Home extends React.PureComponent<Props, State> {
+    state = {
+      authenticated: null,
+      userinfo: ({} as IUser) || null,
+      isOpen: false,
+    };
+
+    checkAuthentication = async () => {
+      const authenticated = await this.props.isAuthenticated();
+      if (authenticated !== this.state.authenticated) {
+        if (authenticated && !this.state.userinfo) {
+          const userinfo = await this.props.getUser();
+          this.setState({
+            authenticated,
+            userinfo,
+          });
+        } else {
+          this.setState({ authenticated });
+        }
+      }
+    };
+
+    componentDidMount = async () => {
+      this.checkAuthentication();
+    };
+
+    componentDidUpdate = async () => {
+      this.checkAuthentication();
+    };
+
+    login = async () => {
+      this.props.auth.login('/');
+    };
+
+    logout = async () => {
+      this.props.auth.logout('/');
+      this.setState({
+        authenticated: null,
+        userinfo: null,
+      });
+    };
+
+    render() {
+      const { authenticated, userinfo } = this.state;
+      if (authenticated === null) {
+        return null;
+      }
+      const button = authenticated ? (
+        <div>
+          <Button color="link">
+            <Link to="/points">Manage Points</Link>
+          </Button>
+          <Button color="link" onClick={this.logout}>
+            Logout
+          </Button>
+        </div>
+      ) : (
+        <Button color="primary" onClick={this.login}>
+          Login
+        </Button>
+      );
+
+      const message = userinfo ? (
+        <p>Hello, {userinfo.given_name}!</p>
+      ) : (
+        <p>Please log in to manage your points.</p>
+      );
+
+      return (
+        <div>
+          <AppNavbar />
+          <Container fluid={true}>
+            {message}
+            {button}
+          </Container>
+        </div>
+      );
+    }
+  },
+);
--- react-client/src/index.tsx
+++ react-client/src/index.tsx
@@ -1,9 +1,12 @@
+// @ts-ignore
+import { Security } from '@okta/okta-react';
 import ApolloClient from 'apollo-boost';
 import * as React from 'react';
 import { ApolloProvider } from 'react-apollo';
 import * as ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-import App from './App';
+import Home from './Home';
 import registerServiceWorker from './registerServiceWorker';
 
 const client = new ApolloClient({
@@ -11,9 +14,32 @@ const client = new ApolloClient({
   credentials: 'include',
 });
 
+// @ts-ignore
+const onAuthRequired = ({ history }) => {
+  history.push('/login');
+};
+
+const baseUrl = 'https://dev-316185.oktapreview.com';
+
+const OktaApp = () => (
+  <Router>
+    <Security
+      issuer={`${baseUrl}/oauth2/default`}
+      client_id={process.env.CLIENT_ID}
+      redirect_uri={window.location.origin + '/implicit/callback'}
+      onAuthRequired={onAuthRequired}
+    >
+      <Route path="/" exact={true} component={Home} />
+      <SecureRoute path="/points" component={Points} />
+      <Route path="/login" render={() => <Login baseUrl={`${baseUrl}`} />} />
+      <Route path="/implicit/callback" component={ImplicitCallback} />
+    </Security>
+  </Router>
+);
+
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <App />
+    <OktaApp />
   </ApolloProvider>,
   document.getElementById('root') as HTMLElement,
 );
